18cb5f922014eb1d8275f1b50570592d4e86f74c
Mohammad <m.mirgolbabaei666@gmail.com>
2023-05-05 11:15:02 +0330

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -2,11 +2,12 @@
 #include<conio.h>
 using namespace std;
 main(){
-	int a,b=0,c=1,d=0;
-	cout<<"How many fibonacci do you wanna see?: ";
+	int a,b=0,c=1;
+	cout<<"Enter number: ";
 	cin>>a;
-	while(++d<=a){
-		cout<<c<<endl;
+	cout<<"Fibonacci's upto "<<a<<" are:"<<endl;
+	while(b<=a){
+		cout<<b<<endl;
 		c+=b;
 		b=c-b;
 	}
